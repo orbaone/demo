ef1f5aece2a7c63d23f4e157ac507bb8115332f3
Jordan Jones <proxima.aust@gmail.com>
2020-10-30 15:11:18 -0500

    feat, add web-sdk libs to source

---
+++ lib/index.umd.d.ts
@@ -0,0 +1,13 @@
+interface OrbaOneConfig {
+    target: string | Element | HTMLElement;
+    apiKey: string;
+    applicantId: string;
+    disableStyle?: boolean;
+    onSuccess: (result: any) => void;
+    onError: (result: any) => void;
+    onCancelled: (result: string) => void;
+    onChange?: (result: string) => void;
+    steps: string[];
+}
+declare function renderButton(config: OrbaOneConfig): void;
+export { renderButton, OrbaOneConfig };
