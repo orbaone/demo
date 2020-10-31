9d7093812c39da00e98431cf24333d280fb80ce5
Jordan Jones <proxima.aust@gmail.com>
2020-10-31 15:13:42 -0500

    patch, change to bloom id

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@ export default {
           const data = await response.json();
           renderButton({
             apiKey: "7f28e4ecf15449888649f28a7a7ab8cb",
-            applicantId: `${data.applicantId}`,
+            applicantId: `${data.applicant_id}`,
             target: "#button",
             onCancelled: data => {
               console.log(data);
