01a816e74af2110dc0fd0388e91e1b230f421b9f
Jordan Jones <proxima.aust@gmail.com>
2021-01-30 16:33:11 -0500

    feat, added @orbaone/api

--- api/createApplicant.js
+++ api/createApplicant.js
@@ -1,33 +1,22 @@
-import fetch from "node-fetch";
+import { OrbaOne } from "@orbaone/api";
 
 module.exports = async (req, res) => {
   const apiKey = process.env.API_KEY;
   const secretKey = process.env.SECRET_KEY;
   const { firstName, middleName, lastName } = req.body;
+
+  const client = new OrbaOne({
+    apiKey,
+    apiSecret: secretKey
+  });
   try {
-    const response = await fetch(
-      "https://api.orbaone.com/api/v1/applicants/create",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          ApiKey: apiKey,
-          Secret: secretKey
-        },
-        body: JSON.stringify({
-          firstName,
-          middleName,
-          lastName
-        })
-      }
-    );
-    if (response.status === 200) {
-      const json = await response.json();
-      res.send(json);
-    } else {
-      throw new Error("Applicant could not be created");
-    }
+    const result = await client.createApplicant({
+      firstName,
+      middleName,
+      lastName
+    });
+
+    res.send(200).json(result);
   } catch (error) {
     res.status(500).json({
       message: `Applicant could not be created, try checking out https://docs.orbaone.com/rest-api/endpoints`
